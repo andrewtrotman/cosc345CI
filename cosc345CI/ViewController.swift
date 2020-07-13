98d1c2afde4cd17b90df40ddc0a543c545ef995c
Andrew Trotman <andrew@trotman.com>
2020-07-13 16:41:30 +1200

    Initial Commit

---
+++ cosc345CI/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  cosc345CI
+//
+//  Created by Andrew Trotman on 13/07/20.
+//  Copyright © 2020 Andrew Trotman. All rights reserved.
+//
+
+import UIKit
+
+public class ViewController: UIViewController {
+
+	public override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+	/**
+		Multiple function exmaple (x * y)
+		- Parameters:
+			- x: [in] The first parameter
+			- y: [in] The second parameter
+		- Returns: The two parameters multiplied by each other
+	*/
+	public static func mymethod(x : Int, y : Int) -> Int
+		{
+		return x * y
+		}
+
+
+}
+
---
+++ cosc345CITests/cosc345CITests.swift
@@ -0,0 +1,34 @@
+//
+//  cosc345CITests.swift
+//  cosc345CITests
+//
+//  Created by Andrew Trotman on 13/07/20.
+//  Copyright © 2020 Andrew Trotman. All rights reserved.
+//
+
+import XCTest
+@testable import cosc345CI
+
+class cosc345CITests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+		  print("AT:textExample tests starting now")
+		  XCTAssert(ViewController.mymethod(x: 5, y: 5) == 25, "Not Good");
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
